205bd9502f52cc38a73c2ee868b89b13eac5feec
theo <theo.saint-amand@orange.fr>
2025-11-05 11:23:45 +0100

    fix: merge conflicts

--- app/src/main/java/com/example/scanner/MainActivity.kt
+++ app/src/main/java/com/example/scanner/MainActivity.kt
@@ -4,39 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.unit.dp
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import com.example.scanner.ui.theme.ScannerTheme
-import coil3.compose.AsyncImage
-import io.paperdb.Paper
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        Paper.init(this)
         setContent {
             ScannerTheme {
                 // 26000001 c'est l'id d'une des cartes, oui c'est chelou me demandez pas pk
@@ -45,8 +19,4 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-
-// Faudra passer tout ça dans un ViewModel et refaire bien les UiStates comme dans le cours
-// là j'ai fait ça rapidement juste pour voir si ça fonctionnait
 
