33205ea4b7065d184920c7a9769ab3797a21131c
theo <theo.saint-amand@orange.fr>
2025-11-05 11:28:52 +0100

    fix: add paper init

--- app/src/main/java/com/example/scanner/MainActivity.kt
+++ app/src/main/java/com/example/scanner/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.scanner.ui.theme.ScannerTheme
+import io.paperdb.Paper
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        Paper.init(this)
         setContent {
             ScannerTheme {
                 // 26000001 c'est l'id d'une des cartes, oui c'est chelou me demandez pas pk
